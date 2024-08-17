2aec629b877de65cdced193991f0b045406115b4
naoto takeda <naoto72725454@gmail.com>
2024-08-17 20:07:20 +0900

    13 Components
    コンポーネントを作成し、呼び出しまでの仕組み
    <x-component-name />でcomponent-nameを呼び出せる
    <x-alert type="error" message="Something went wrong!" />でデータを受け渡す事もできる
    上記の際、View/Componentsで作成したPHPファイルのクラスで呼び出し方を定義する

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,32 @@
+<x-message-banner :msg="'user signup has been done'" class="success"/>
+<x-message-banner :msg="'server is down'" class="error"/>
+<x-message-banner :msg="'no product found'" class="warning"/>
+
 <h1>
     About Page
 </h1>
 @include('common.inner', ['page' => 'this is about page'])
+
+<style>
+    .success{
+        background-color: lightgreen;
+        color: green;
+        padding: 3px 10px;
+        display: inline-block;
+        border-radius: 2px;
+    }
+    .error{
+        background-color: darkred;
+        color: red;
+        padding: 3px 10px;
+        display: inline-block;
+        border-radius: 2px;
+    }
+    .warning{
+        background-color: darkorange;
+        color: orange;
+        padding: 3px 10px;
+        display: inline-block;
+        border-radius: 2px;
+    }
+</style>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,33 @@
 @include('common.header')
 
+<x-message-banner :msg="'user added succesfully'" class="success"/>
+<x-message-banner :msg="'user longout succesfully'" class="success"/>
+<x-message-banner :msg="'no user found'" class="error"/>
+<x-message-banner :msg="'some users are missing'" class="warning"/>
 <h1>Home Page</h1>
+
 @includeIf('common.inner', ['page' => 'this is home page'])
 
+<style>
+    .success{
+        background-color: lightgreen;
+        color: green;
+        padding: 3px 10px;
+        display: inline-block;
+        border-radius: 2px;
+    }
+    .error{
+        background-color: darkred;
+        color: red;
+        padding: 3px 10px;
+        display: inline-block;
+        border-radius: 2px;
+    }
+    .warning{
+        background-color: darkorange;
+        color: orange;
+        padding: 3px 10px;
+        display: inline-block;
+        border-radius: 2px;
+    }
+</style>
